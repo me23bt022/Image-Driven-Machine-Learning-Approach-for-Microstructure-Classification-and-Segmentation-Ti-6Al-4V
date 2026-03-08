2036f34ddd6280c7acab983331a94d5f57726e30
me23bt022 <me23bt022@iitdh.ac.in>
2026-03-08 15:37:56 +0530

    Update main.py

--- src/main.py
+++ src/main.py
@@ -5,11 +5,18 @@ Author: Arun Baskaran
 """
 
 # Testing edits
+import os
+os.environ["TF_CPP_MIN_LOG_LEVEL"] = "3"
+
+import logging
+logging.getLogger("tensorflow").setLevel(logging.ERROR)
 
 import lib_imports
 from aux_funcs import *
 import model_params
 import sys
+import warnings
+warnings.filterwarnings("ignore")
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
@@ -27,7 +34,7 @@ if __name__ == "__main__":
     test_accuracy(model, test_images, test_labels)
     
     y_classes = get_predicted_classes(model, test_images)
-    
+    print(y_classes)
     feature_segmentation(y_classes)
     
 
