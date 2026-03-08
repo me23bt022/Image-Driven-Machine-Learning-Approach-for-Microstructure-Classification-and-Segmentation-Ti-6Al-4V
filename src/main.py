f66b8f680dabd2bc4a5f9641e6790e479b5bda68
me23bt022 <me23bt022@iitdh.ac.in>
2026-03-08 15:17:16 +0530

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,48 +1,35 @@
 """
 ----------------------------------ABOUT-----------------------------------
 Author: Arun Baskaran
-Modernized for TensorFlow 2.x
----------------------------------------------------------------------------
+--------------------------------------------------------------------------
 """
 
-import sys
+# Testing edits
+
+import lib_imports
+from aux_funcs import *
 import model_params
-from aux_funcs import (
-    load_images_labels,
-    train_model,
-    load_model,
-    test_accuracy,
-    get_predicted_classes,
-    feature_segmentation
-)
-
-
-def main(mode="training"):
-
-    # Load dataset
-    X_train, y_train, X_test, y_test, X_val, y_val = load_images_labels()
-
-    if mode == "training":
-        model, history = train_model(X_train, y_train, X_val, y_val)
-
-    elif mode == "load":
-        model = load_model()
-
-    else:
-        raise ValueError("Mode must be 'training' or 'load'")
-
-    # Evaluate model
-    test_accuracy(model, X_test, y_test)
-
-    # Get predictions
-    y_classes = get_predicted_classes(model, X_test)
-
-    # Run segmentation
-    feature_segmentation(y_classes)
-
+import sys
 
 if __name__ == "__main__":
-    if len(sys.argv) == 2:
-        main(sys.argv[1])
-    else:
-        main("training")
+    if len(sys.argv) != 2:
+        print("Usage: main <mode>", file=sys.stderr)
+        sys.exit(-1)
+    mode = sys.argv[1] 
+    train_images, train_labels, test_images, test_labels, validation_images, validation_labels = load_images_labels()
+    
+    if mode == "training" :
+        model = train_model()
+    
+    elif mode =="load":
+        model = load_model()
+        
+    test_accuracy(model, test_images, test_labels)
+    
+    y_classes = get_predicted_classes(model, test_images)
+    
+    feature_segmentation(y_classes)
+    
+
+
+
